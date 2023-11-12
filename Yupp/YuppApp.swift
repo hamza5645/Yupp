8bd5dcd2d4f1433cdb411e11b5dcb3f82aec469f
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-11-12 14:35:59 +0200

    Initial Commit

---
+++ Yupp/YuppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuppApp.swift
+//  Yupp
+//
+//  Created by Hamza Osama on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct YuppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
