dc2a33e9fdd82b5f506bed867ad5f600d9a84a9d
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-11-13 11:39:16 +0200

    Creating, Editing and deleting Tasks

---
+++ Yupp/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  Yupp
+//
+//  Created by Hamza Osama on 13/11/2023.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
