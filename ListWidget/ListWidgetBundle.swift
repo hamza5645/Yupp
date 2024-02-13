bf11871524fa1cf1538218af4e8209d9b3dd9509
hamza5645 <hamzaosama5645@gmail.com>
2024-02-13 19:26:55 +0200

    added widgetKit target

---
+++ ListWidget/ListWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ListWidgetBundle.swift
+//  ListWidget
+//
+//  Created by Hamza Osama on 2/13/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ListWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ListWidget()
+    }
+}
